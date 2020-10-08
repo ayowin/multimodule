86bcea135df356f038f40e6b9b1b3e46be82d433
OuyangWei <18344175265@126.com>
2020-10-08 17:42:52 +0800

    resolve mvn package bug, now mvn package can use for multimodule project which is the root project

--- restful-order/src/main/java/com/ouyangwei/multimodule/RestfulOrderApplication.java
+++ restful-order/src/main/java/com/ouyangwei/multimodule/RestfulOrderApplication.java
@@ -1,6 +1,5 @@
 package com.ouyangwei.multimodule;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
